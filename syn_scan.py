63884cf8aa2b712cd1b908dcbdfcc5f54bc40f67
isra <isra@localhost.localdomain>
2024-07-19 20:21:35 -0600

    Se agrego funcion ctrlC

--- syn_scan.py
+++ syn_scan.py
@@ -3,39 +3,48 @@ import threading
 import sys
 from datetime import datetime
 
+# Función para escanear un puerto específico
 def scan_port(target, port):
-    syn_packet = IP(dst=target) / TCP(dport=port, flags='S')
-    response = sr1(syn_packet, timeout=1, verbose=False)
-
-    if response:
-        if response.haslayer(TCP):
-            if response[TCP].flags == 'SA':  # SYN-ACK
-                print(f"Puerto {port} está abierto")
-                rst_packet = IP(dst=target) / TCP(dport=port, flags='R')
-                send(rst_packet, verbose=False)
-            elif response[TCP].flags == 'RA':  # RST-ACK
-                print(f"Puerto {port} está cerrado")
-                
+    try:
+        syn_packet = IP(dst=target) / TCP(dport=port, flags='S')
+        response = sr1(syn_packet, timeout=1, verbose=False)
+        if response:
+            if response.haslayer(TCP):
+                if response[TCP].flags == 'SA':  # SYN-ACK
+                    print(f"Puerto {port} está abierto")                		#	 Enviar un paquete RST para restablecer la conexión
+                    rst_packet = IP(dst=target) / TCP(dport=port, flags='R')
+                    send(rst_packet, verbose=False)
+    except KeyboardInterrupt:
+        print('ol')
+        exit(0)
 
 def syn_scan(target, start_port, end_port, max_threads=100):
-    print('-' * 50)
-    print(f"Escaneando la ip: {target} \n\n" "Escaneo iniciado en: " + str(datetime.now()))
-    print('-' * 50)
+    try:
+        print('-' * 50)
+        print(f"Escaneando la ip: {target} \n\n" "Escaneo iniciado en: " + str(datetime.now()))
+        print('-' * 50)
 
-    threads = []
+    # Lista para almacenar los hilos
+        threads = []
 
-    for port in range(start_port, end_port + 1):
-        t = threading.Thread(target=scan_port, args=(target, port))
-        threads.append(t)
-        t.start()
+        for port in range(start_port, end_port + 1):
+        # Crear un hilo para cada puerto
+            t = threading.Thread(target=scan_port, args=(target, port))
+            threads.append(t)
+            t.start()
 
-        if len(threads) >= max_threads:
-            for t in threads:
-                t.join()
-            threads = []
+        # Limitar el número de hilos en ejecución simultáneamente
+            if len(threads) >= max_threads:
+                for t in threads:
+                    t.join()
+                threads = []
 
-    for t in threads:
-        t.join()
+    # Esperar a que todos los hilos terminen
+        for t in threads:
+            t.join()
+    except KeyboardInterrupt:
+            print('Programa finalizado.')
+            exit(0)
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
